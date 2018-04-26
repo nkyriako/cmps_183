7763e2582ca742f9f0cd505b30c111839bb5225f
mlapo <mflapoin@ucsc.edu>
2018-04-25 19:23:13 -0700

    added banner table

--- web2py/applications/AstroChat/models/tables.py
+++ web2py/applications/AstroChat/models/tables.py
@@ -13,14 +13,19 @@ def get_user_email():
     return auth.user.email if auth.user is not None else None
 
 db.define_table('user_table',
-		        Field('user_email', default=get_user_email()),
+		Field('user_email', default=get_user_email()),
                 Field('birthday', 'datetime'),
-                Field('birth_sign')
+                Field('profile_picture'),
+                Field('bio'),
+                Field('banner_id', default='1')
                 )
 
-db.user_table.user_email.writeable = db.user_table.user_email.readable = False
-db.user_table.id.writeable = db.user_table.id.readable = False
-db.user_table.birth_sign.writeable = db.user_table.birth_sign.readable = False
+db.user_table.user_email.writable = db.user_table.user_email.readable = False
+db.user_table.id.writable = db.user_table.id.readable = False
+
+db.define_table('banners_table',
+		Field('banner_color')
+                )
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
